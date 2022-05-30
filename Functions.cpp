4a82eb75b3833e51378b28aa387a90c0972f84d4
clxyify <98536033+clxyify@users.noreply.github.com>
2022-05-30 19:16:44 -0400

    Update Functions.cpp

--- Functions.cpp
+++ Functions.cpp
@@ -16,3 +16,12 @@ extern int rlua_toboolean (DWORD rL, int idx) {//Converts the Lua value at the g
   const TValue *o = r_lua_index2adr(rL, idx);
   return !l_isfalse(o);//If errors, go to lobject.h and copy the refrences
 }
+
+extern int rlua_type (DWORD rL, int idx) {//Returns the type of the value in the given valid index
+  StkId o = r_lua_index2adr(rL, idx);//External type, not a lua API function
+  return (o == luaO_nilobject) ? -1 : ttype(o);//Requires some lua files
+}
+
+extern const char *lua_typename (DWORD rL, int t) {
+  return (t == LUA_TNONE) ? "no value" : luaT_typenames[t];
+}
