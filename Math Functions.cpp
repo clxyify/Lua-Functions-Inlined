b4027b8599f1d2a31c0ff2265e1885c8c69f4c7d
clxyify <98536033+clxyify@users.noreply.github.com>
2022-06-04 20:51:14 -0400

    Update Math Functions.cpp

--- Math Functions.cpp
+++ Math Functions.cpp
@@ -1,6 +1,8 @@
 //math library functions if you dont know what it is
 //roblox technically becomes a nerd and does math so here ya go
 //it should work and u can modify it and skid it so idrc but here you go
+//btw to register use your custom register env system and anyways enjoy!
+
 static float fade(float t)//bye
 {
     return t * t * t * (t * (t * 6 - 15) + 10);
@@ -34,7 +36,7 @@ static int math_noise(DWORD rL)//math noise
     return 1;
 }
 
-static int math_clamp(DWORD rL)//clamp
+static int math_clamp(DWORD rL)//clamp function
 {
     double v = r_luaL_checknumber(rL, 1);//DWORD a1, int a2
     double min = r_luaL_checknumber(rL, 2);
@@ -49,24 +51,57 @@ static int math_clamp(DWORD rL)//clamp
     return 1;
 }
 
-static int math_sign(DWORD rL)//sign
+static int math_sign(DWORD rL)//sign function
 {
     double v = r_luaL_checknumber(rL, 1);//todo: i hate v
     r_lua_pushnumber(rL, v > 0.0 ? 1.0 : v < 0.0 ? -1.0 : 0.0);//time to get ur brain fucked :D
     return 1;
 }
 
-static int math_max(lua_State* L)
+static int math_max(DWORD rL)//math max function
 {
-    int n = lua_gettop(L); /* number of arguments */
-    double dmax = luaL_checknumber(L, 1);
+    int n = r_lua_gettop(rL); /* number of arguments */
+    int nn = 1;
+    double dmax = r_luaL_checknumber(rL, nn);
     int i;
     for (i = 2; i <= n; i++)
     {
-        double d = luaL_checknumber(L, i);
+        double d = r_luaL_checknumber(rL, i);
         if (d > dmax)
             dmax = d;
     }
-    lua_pushnumber(L, dmax);
+    r_lua_pushnumber(rL, dmax);//push number to r_luaL_checknumber(1)
+    return 1;
+}
+
+static int math_min(DWORD  rL)//math min function (minimum)
+{
+    int n = r_lua_gettop(rL); /* number of arguments */
+    double dmin = r_luaL_checknumber(rL, 1);
+    int i;
+    for (i = 2; i <= n; i++)
+    {
+        double d = r_luaL_checknumber(rL, i);
+        if (d < dmin)//technically math_max but it checks if dmin is greater
+            dmin = d;
+    }
+    r_lua_pushnumber(rL, dmin);
+    return 1;
+}
+#undef PI
+
+#define PI (3.14159265358979323846)
+#define RADIANS_PER_DEGREE (PI / 180.0)
+
+static int math_deg(DWORD rL)
+{
+    r_lua_pushnumber(rL, r_luaL_checknumber(rL, 1) / RADIANS_PER_DEGREE);
+    return 1;
+}
+
+static int math_rad(DWORD rL)
+{
+    int check = 1;
+    r_lua_pushnumber(rL, r_luaL_checknumber(rL, check) * RADIANS_PER_DEGREE);//multiply wow
     return 1;
 }
