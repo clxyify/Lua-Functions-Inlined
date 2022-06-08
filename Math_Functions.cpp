f1b18ccf948e5e26ae7a2fca02d08f942ddf5954
clxyify <98536033+clxyify@users.noreply.github.com>
2022-06-08 16:31:33 -0400

    Update Math_Functions.cpp

--- Math_Functions.cpp
+++ Math_Functions.cpp
@@ -22,6 +22,111 @@ static float grad(unsigned char hash, float x, float y, float z)//grad vector3?
     return (h & 1 ? -u : u) + (h & 2 ? -v : v);
 }
 
+static int math_abs(DWORD rL)
+{
+    r_lua_pushnumber(rL, fabs(r_luaL_checknumber(rL, 1)));
+    return 1;
+}
+
+static int math_sin(DWORD rL)
+{
+    r_lua_pushnumber(rL, sin(r_luaL_checknumber(rL, 1)));
+    return 1;
+}
+
+static int math_sinh(DWORD rL)
+{
+    r_lua_pushnumber(rL, sinh(r_luaL_checknumber(rL, 1)));
+    return 1;
+}
+
+static int math_cos(DWORD rL)
+{
+    r_lua_pushnumber(rL, cos(r_luaL_checknumber(rL, 1)));
+    return 1;
+}
+
+static int math_cosh(DWORD rL)
+{
+    r_lua_pushnumber(rL, cosh(r_luaL_checknumber(rL, 1)));
+    return 1;
+}
+
+static int math_tan(DWORD rL)
+{
+    r_lua_pushnumber(rL, tan(r_luaL_checknumber(rL, 1)));
+    return 1;
+}
+
+static int math_tanh(DWORD rL)
+{
+    r_lua_pushnumber(rL, tanh(r_luaL_checknumber(rL, 1)));
+    return 1;
+}
+
+static int math_asin(DWORD rL)
+{
+    r_lua_pushnumber(rL, asin(r_luaL_checknumber(rL, 1)));
+    return 1;
+}
+
+static int math_acos(DWORD rL)
+{
+    r_lua_pushnumber(rL, acos(r_luaL_checknumber(rL, 1)));
+    return 1;
+}
+
+static int math_atan(DWORD rL)
+{
+    r_lua_pushnumber(rL, atan(r_luaL_checknumber(rL, 1)));
+    return 1;
+}
+
+static int math_atan2(DWORD rL)
+{
+    r_lua_pushnumber(rL, atan2(r_luaL_checknumber(rL, 1), r_luaL_checknumber(rL, 2)));
+    return 1;
+}
+
+static int math_ceil(DWORD rL)
+{
+    r_lua_pushnumber(rL, ceil(r_luaL_checknumber(rL, 1)));
+    return 1;
+}
+
+static int math_floor(DWORD rL)
+{
+    r_lua_pushnumber(rL, floor(r_luaL_checknumber(rL, 1)));
+    return 1;
+}
+
+static int math_fmod(DWORD rL)
+{
+    r_lua_pushnumber(rL, fmod(r_luaL_checknumber(rL, 1), r_luaL_checknumber(rL, 2)));
+    return 1;
+}
+
+static int math_modf(DWORD rL)
+{
+    double ip;
+    double fp = modf(r_luaL_checknumber(rL, 1), &ip);
+    r_lua_pushnumber(rL, ip);
+    r_lua_pushnumber(rL, fp);
+    return 2;
+}
+
+static int math_sqrt(DWORD rL)
+{
+    r_lua_pushnumber(rL, sqrt(r_luaL_checknumber(rL, 1)));
+    return 1;
+}
+
+static int math_pow(DWORD rL)//pow pow
+{
+    r_lua_pushnumber(rL, pow(r_luaL_checknumber(rL, 1), r_luaL_checknumber(rL, 2)));
+    return 1;
+}
+
 static int math_noise(DWORD rL)//math noise
 {
     //for the stupids out their, optnumber = lual_optinteger
@@ -105,3 +210,58 @@ static int math_rad(DWORD rL)
     r_lua_pushnumber(rL, r_luaL_checknumber(rL, check) * RADIANS_PER_DEGREE);//multiply wow
     return 1;
 }
+
+static const unsigned char kPerlin[512] = {151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140, 36, 103, 30, 69, 142, 8, 99,
+    37, 240, 21, 10, 23, 190, 6, 148, 247, 120, 234, 75, 0, 26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33, 88, 237, 149, 56, 87, 174,
+    20, 125, 136, 171, 168, 68, 175, 74, 165, 71, 134, 139, 48, 27, 166, 77, 146, 158, 231, 83, 111, 229, 122, 60, 211, 133, 230, 220, 105, 92, 41,
+    55, 46, 245, 40, 244, 102, 143, 54, 65, 25, 63, 161, 1, 216, 80, 73, 209, 76, 132, 187, 208, 89, 18, 169, 200, 196, 135, 130, 116, 188, 159, 86,
+    164, 100, 109, 198, 173, 186, 3, 64, 52, 217, 226, 250, 124, 123, 5, 202, 38, 147, 118, 126, 255, 82, 85, 212, 207, 206, 59, 227, 47, 16, 58, 17,
+    182, 189, 28, 42, 223, 183, 170, 213, 119, 248, 152, 2, 44, 154, 163, 70, 221, 153, 101, 155, 167, 43, 172, 9, 129, 22, 39, 253, 19, 98, 108, 110,
+    79, 113, 224, 232, 178, 185, 112, 104, 218, 246, 97, 228, 251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 241, 81, 51, 145, 235, 249, 14,
+    239, 107, 49, 192, 214, 31, 181, 199, 106, 157, 184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254, 138, 236, 205, 93, 222, 114, 67, 29, 24,
+    72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180,
+
+    151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140, 36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23, 190, 6, 148, 247,
+    120, 234, 75, 0, 26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33, 88, 237, 149, 56, 87, 174, 20, 125, 136, 171, 168, 68, 175, 74,
+    165, 71, 134, 139, 48, 27, 166, 77, 146, 158, 231, 83, 111, 229, 122, 60, 211, 133, 230, 220, 105, 92, 41, 55, 46, 245, 40, 244, 102, 143, 54, 65,
+    25, 63, 161, 1, 216, 80, 73, 209, 76, 132, 187, 208, 89, 18, 169, 200, 196, 135, 130, 116, 188, 159, 86, 164, 100, 109, 198, 173, 186, 3, 64, 52,
+    217, 226, 250, 124, 123, 5, 202, 38, 147, 118, 126, 255, 82, 85, 212, 207, 206, 59, 227, 47, 16, 58, 17, 182, 189, 28, 42, 223, 183, 170, 213,
+    119, 248, 152, 2, 44, 154, 163, 70, 221, 153, 101, 155, 167, 43, 172, 9, 129, 22, 39, 253, 19, 98, 108, 110, 79, 113, 224, 232, 178, 185, 112,
+    104, 218, 246, 97, 228, 251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 241, 81, 51, 145, 235, 249, 14, 239, 107, 49, 192, 214, 31, 181, 199,
+    106, 157, 184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254, 138, 236, 205, 93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61,
+    156, 180};
+
+static float perlin(float x, float y, float z)//imagine vector3
+{
+    float xflr = floorf(x);
+    float yflr = floorf(y);
+    float zflr = floorf(z);
+
+    int xi = int(xflr) & 255;
+    int yi = int(yflr) & 255;
+    int zi = int(zflr) & 255;
+
+    float xf = x - xflr;
+    float yf = y - yflr;
+    float zf = z - zflr;
+
+    float u = fade(xf);
+    float v = fade(yf);
+    float w = fade(zf);
+
+    const unsigned char* p = kPerlin;
+
+    int a = p[xi] + yi;
+    int aa = p[a] + zi;
+    int ab = p[a + 1] + zi;
+
+    int b = p[xi + 1] + yi;
+    int ba = p[b] + zi;
+    int bb = p[b + 1] + zi;
+
+    return lerp(w,
+        lerp(v, lerp(u, grad(p[aa], xf, yf, zf), grad(p[ba], xf - 1, yf, zf)), lerp(u, grad(p[ab], xf, yf - 1, zf), grad(p[bb], xf - 1, yf - 1, zf))),
+        lerp(v, lerp(u, grad(p[aa + 1], xf, yf, zf - 1), grad(p[ba + 1], xf - 1, yf, zf - 1)),
+            lerp(u, grad(p[ab + 1], xf, yf - 1, zf - 1), grad(p[bb + 1], xf - 1, yf - 1, zf - 1))));
+}
+
