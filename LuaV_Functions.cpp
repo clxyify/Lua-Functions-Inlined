bc435116531e90bff868d8924e51fe65e0a78971
clxyify <98536033+clxyify@users.noreply.github.com>
2022-06-18 21:21:38 -0400

    Update LuaV_Functions.cpp

--- LuaV_Functions.cpp
+++ LuaV_Functions.cpp
@@ -4,6 +4,7 @@ vfake int r_luaV_strcmp(const string* ls, const string* rs)//seggsy inlined func
 {
     char data = 0;//this is actually 1 i just put it to 0 for no reason
     if (ls == rs)//ls = rs or rs = ls
+        r_lua_pushnil(rL);//push nil
         return NULL;
 
     const char* l = (ls->c_str()) + data;
@@ -16,7 +17,7 @@ vfake int r_luaV_strcmp(const string* ls, const string* rs)//seggsy inlined func
     if (res != NULL)
         return res;
     else
-        return res;//dont return anything
+        r_lua_pushnil(rL);//aint this a smart method?
 
     return ll == lr ? 0 : ll < lr ? -1 : 1;//math teachers are smart
 }
